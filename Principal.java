0f2885c98ac7c66baf68ebfb7f6ed1cf05c3900a
gabofun <gabo.ctm@gmail.com>
2019-05-31 13:02:18 -0400

    segunda linea

--- Principal.java
+++ Principal.java
@@ -1,10 +1,11 @@
 public class Principal{
 
-public static void main(String[] argas){
+public static void main(String[] args){
 
 
 
 System.out.println("Ejemplo1");
+System.out.println("segunda linea.");
 
 }
 
